452c52677c72a6c65a68dd5b6e5c41065d04fbdf
ruslanalesik <ruserdash7@gmail.com>
2023-01-23 23:01:00 +0100

    refactor: remove unused code

--- db/migrations/2023.01.23T21.25.59.users.table.ts
+++ db/migrations/2023.01.23T21.25.59.users.table.ts
@@ -1,28 +1,8 @@
-import { MigrationFn } from 'umzug';
 import { User, USER_TABLE_NAME } from '../../src/database/models/user';
 import { Migration } from '../umzug';
 
-/*
-export const up: MigrationFn<User> = async () => await User.sync();
-export const down: MigrationFn<User> = async () => await User.drop();
- */
-
-export const up: Migration = async ({ context: sequelize }) => {
-    console.log('up....');
+export const up: Migration = async () => {
     await User.sync({ force: true });
-    /*
-  await sequelize.getQueryInterface().createTable('users', {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    });
-   */
 };
 
 export const down: Migration = async ({ context: sequelize }) => {
